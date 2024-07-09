588305b71b08acd75cf8b7e0ed3982900abfb827
Sanctity2k5 <sanctity2k5@gmail.com>
2024-07-09 19:30:32 +0100

    add security protocols

--- app.js
+++ app.js
@@ -1,20 +1,53 @@
 const express = require("express");
-const path = require('path');
+const path = require("path");
 const productRoutes = require("./routers/productRoute");
 const userRoutes = require("./routers/userRoute");
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const bodyParser = require("body-parser");
+const hpp = require("hpp");
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 // Serving static files
 app.use(express.static(path.join(__dirname, "public")));
+app.use(helmet());
+
+const limiter = rateLimit({
+  max: 100,
+  windowsMs: 60 * 60 * 1000,
+  message: "Too many requests from this Ip, Please try again in an hour",
+});
+app.use("/api", limiter);
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+//Prevent parameter pollution
+app.use(hpp());
+// Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      "duration",
+      "ratingsQuantity",
+      "ratingsAverage",
+      "maxGroupSize",
+      "difficulty",
+      "price",
+    ],
+  })
+);
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   console.log(req.headers);
   next();
 });
 // Middleware to parse JSON bodies
-app.use(express.json());
+app.use(express.json({ limit: "10kb" }));
 
 // Use the product routes
 app.use("/api", productRoutes);
